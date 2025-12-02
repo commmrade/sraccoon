2f935566c704cc7267649dee1b8325b56139fd0e
commmrade <cigankov807@gmail.com>
2025-12-02 19:02:13 +0300

    Fixed bug in send command, wrote a bt more todos

--- src/main.zig
+++ src/main.zig
@@ -43,6 +43,17 @@ fn getPasswordFromInp(stdin: *std.io.Reader, alloc: std.mem.Allocator) ![]u8 {
     return command_str;
 }
 
+fn getCommandFromInp(stdin: *std.io.Reader, alloc: std.mem.Allocator) ![]u8 {
+    std.debug.print("Enter a command: ", .{});
+    const input = try stdin.takeDelimiterExclusive('\n');
+
+    const command_str = try alloc.alloc(u8, input.len + 2);
+    @memcpy(command_str[0..input.len], input);
+    command_str[command_str.len - 2] = 0;
+    command_str[command_str.len - 1] = 0;
+    return command_str;
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = gpa.allocator();
@@ -54,10 +65,10 @@ pub fn main() !void {
 
     var rbuf: [1024]u8 = undefined;
 
-    var stdin_reader = std.fs.File.stdin().reader(&rbuf);
-    const stdin = &stdin_reader.interface;
+    var pswd_reader = std.fs.File.stdin().reader(&rbuf);
+    const pswd_stdin = &pswd_reader.interface;
 
-    const rcon_pass = try getPasswordFromInp(stdin, alloc);
+    const rcon_pass = try getPasswordFromInp(pswd_stdin, alloc);
 
     const auth_packet = RconPacket{ .size = @intCast(RCON_PACKET_MIN_SIZE + rcon_pass.len - 2), .id = SERVERDATA_AUTH_ID, .type = 3 };
 
@@ -73,33 +84,29 @@ pub fn main() !void {
         std.debug.print("Was not authorized", .{});
         return;
     }
+    // TODO: Handle errors gracefully
 
     std.debug.print("Authorized\n", .{});
     // std.debug.print("Response: {}\n", .{auth_response_packet});
 
     while (true) {
-        @memset(&rbuf, 0);
+        var cmd_reader = std.fs.File.stdin().reader(&rbuf);
+        const cmd_stdin = &cmd_reader.interface;
 
-        std.debug.print("Enter a command: ", .{});
-        const input = try stdin.takeDelimiterExclusive('\n');
-        // std.debug.print("Cm: '{s}' {}\n", .{ input, input.len });
-        const command_str = try alloc.alloc(u8, input.len + 2);
+        const command_str = try getCommandFromInp(cmd_stdin, alloc);
         defer alloc.free(command_str);
-        // FIXME: Reads 1 byte more than supposed to
-        @memcpy(command_str[0..input.len], input);
-        command_str[command_str.len - 2] = 0;
-        command_str[command_str.len - 1] = 0;
 
         const command_packet = RconPacket{ .size = @intCast(RCON_PACKET_MIN_SIZE + command_str.len - 2), .id = SEND_COMMAND_ID, .type = 2 };
         const command_packet_b = try command_packet.build(command_str, alloc);
-        std.debug.print("Cm: '{s}' {}\n", .{ command_str, command_str.len });
         _ = try std.posix.write(sock, command_packet_b); // TODO: What if cant write in 1 write, need to split it
-        // std.debug.print("Written comm bytes: {}, {}, {}\n", .{ wr_bytes, command_packet, command_packet_b.len });
 
         const rd_bytes = try std.posix.read(sock, &rbuf); // TODO: This may not come in 1 read, so need a way to make sure it all comes, timeout?
 
-        // const command_response_packet = std.mem.bytesToValue(RconPacket, rbuf[0 .. @bitSizeOf(RconPacket) / 8]);
-        const left_bytes = rd_bytes - @bitSizeOf(RconPacket) / 8;
-        std.debug.print("Command response: {s}", .{rbuf[@bitSizeOf(RconPacket) / 8 .. @bitSizeOf(RconPacket) / 8 + left_bytes]});
+        if (rd_bytes < @bitSizeOf(RconPacket) / 8) {
+            std.debug.print("Malformed response\n", .{});
+        } else {
+            const left_bytes: usize = rd_bytes - @bitSizeOf(RconPacket) / 8;
+            std.debug.print("Command response: {s}\n", .{rbuf[@bitSizeOf(RconPacket) / 8 .. @bitSizeOf(RconPacket) / 8 + left_bytes]});
+        }
     }
 }
